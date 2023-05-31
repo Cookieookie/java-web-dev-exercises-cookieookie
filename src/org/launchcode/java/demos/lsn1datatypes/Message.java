1255786c71ea0d046cc340e8ed93639db05e6045
Cookie Van <cookievan03@gmail.com>
2023-05-30 23:54:53 -0500

    finished exercises up to 4

--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("gh")) {
+          return "Ni hao!";
         } else {
             return "Hello, World!";
         }
