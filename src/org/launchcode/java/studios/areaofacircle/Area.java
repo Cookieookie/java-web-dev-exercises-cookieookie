40e29e372da911b93ed3c8d9f89c3d31086c353b
Cookie Van <cookievan03@gmail.com>
2023-08-16 16:49:14 -0500

    changed areaofacircle studio with Math.PI and changed CountingCharacters studio to work

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -2,6 +2,8 @@ package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
 
+import static java.lang.Double.isNaN;
+
 public class Area {
     public static void main(String[] args) {
         double radius;
@@ -10,9 +12,17 @@ public class Area {
 
         System.out.println("Enter a radius:");
         radius = input.nextDouble();
-        input.close();
+        input.close(); // this closes the scanner or input source
 //        area = 3.14 * radius * radius;
         area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+
+        // bonus mission 1
+        if (radius < 0) {
+            System.err.println("That is a negative number. Please try again.");
+        } else if (Double.toString(radius) == null) {
+            System.err.println("This is not a number. Please try again.");
+        } else {
+                System.out.println("The area of a circle of radius " + radius + " is: " + area);
+            }
+        }
     }
-}
--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -1,28 +1,50 @@
 package org.launchcode.java.studios.countingcharacters;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+
 public class CountingCharacters {
-    public static void main (String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        String quote ="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        String quote="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        Map <Character, Integer>charMapCount = new HashMap<>();
 
-        char[] charactersInString = quote.toCharArray();
-        HashMap<Character, Integer> letterCounts = new HashMap<>();
+        for(Character letter : quote.toCharArray()) {
 
-        for (char letter: charactersInString) {
-            if (letterCounts.containsKey(letter)) {
-                letterCounts.put(letter, 1);
+            if(charMapCount.containsKey(letter)) {
+                charMapCount.put(letter, charMapCount.get(letter) + 1);
             } else {
-//                letterCounts.computeIfPresent(letter,
-//                (key, val) -> val + 1);
-
-                 letterCounts.put(letter,letterCounts.get(letter) + 1);
+                charMapCount.put(letter, 1);
             }
         }
-        for (Map.Entry<Character, Integer> lettersEntry: letterCounts.entrySet()) {
-            System.out.println(lettersEntry.getKey() + ": " + lettersEntry.getValue());
+
+        for (Map.Entry<Character, Integer> entry : charMapCount.entrySet()) {
+            System.out.println(entry.getKey()+" : "+entry.getValue());
         }
     }
 }
+
+
+//public class CountingCharacters {
+//    public static void main (String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        String quote ="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+//
+//        char[] charactersInString = quote.toCharArray();
+//        HashMap<Character, Integer> letterCounts = new HashMap<>();
+//
+//        for (char letter: charactersInString) {
+//            if (letterCounts.containsKey(letter)) {
+//                letterCounts.put(letter, 1);
+//            } else {
+//                letterCounts.computeIfPresent(letter,
+//                (key, val) -> val + 1);
+//
+//                 letterCounts.put(letter,letterCounts.get(letter) + 1);
+//            }
+//        }
+//        for (Map.Entry<Character, Integer> lettersEntry: letterCounts.entrySet()) {
+//            System.out.println(lettersEntry.getKey() + ": " + lettersEntry.getValue());
+//        }
+//    }
+//}
