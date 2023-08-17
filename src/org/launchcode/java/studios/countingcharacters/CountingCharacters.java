343567f13a6c79e1b516ad89f7c02925a528b4b0
Cookie Van <cookievan03@gmail.com>
2023-08-16 19:25:36 -0500

    Added first bonus mission to countingcharacters studio

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -1,11 +1,16 @@
 package org.launchcode.java.studios.countingcharacters;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args) {
+        // bonus mission 1 - Prompt the user to enter the  string in the command line
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word or phrase you would like to count: ");
+        String quote = input.next();
 
-        String quote="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+//        String quote="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         Map <Character, Integer>charMapCount = new HashMap<>();
 
         for(Character letter : quote.toCharArray()) {
@@ -23,7 +28,7 @@ public class CountingCharacters {
     }
 }
 
-
+// from previous KC class - does not work
 //public class CountingCharacters {
 //    public static void main (String[] args) {
 //        Scanner input = new Scanner(System.in);
