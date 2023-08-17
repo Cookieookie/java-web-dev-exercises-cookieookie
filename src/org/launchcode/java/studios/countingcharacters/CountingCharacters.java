fde901ad02bec67fb053f3e030843a639e882904
Cookie Van <cookievan03@gmail.com>
2023-08-16 19:30:59 -0500

    completed bonus mission 2 of countingcharacters studio

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -13,7 +13,7 @@ public class CountingCharacters {
 //        String quote="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         Map <Character, Integer>charMapCount = new HashMap<>();
 
-        for(Character letter : quote.toCharArray()) {
+        for(Character letter : quote.toLowerCase().toCharArray()) { // bonus mission 2 - added .toLowerCase() to make case-insensitive.
 
             if(charMapCount.containsKey(letter)) {
                 charMapCount.put(letter, charMapCount.get(letter) + 1);
