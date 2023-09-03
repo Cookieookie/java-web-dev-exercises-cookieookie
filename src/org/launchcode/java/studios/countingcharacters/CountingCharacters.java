71085f4d795bc95360c3b395af89877357714785
Cookie Van <cookievan03@gmail.com>
2023-09-03 09:09:25 -0500

    finished chapter 6 exercises

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -11,22 +11,29 @@ public class CountingCharacters {
         String quote = input.next();
 
 //        String quote="If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        Map <Character, Integer>charMapCount = new HashMap<>();
 
-        for(Character letter : quote.toLowerCase().toCharArray()) { // bonus mission 2 - added .toLowerCase() to make case-insensitive.
+        Map<Character, Integer> charMapCount = new HashMap<>();
 
-            if(charMapCount.containsKey(letter)) {
-                charMapCount.put(letter, charMapCount.get(letter) + 1);
-            } else {
-                charMapCount.put(letter, 1);
+        for (Character letter : quote.toLowerCase().toCharArray()) { // bonus mission 2 - added .toLowerCase() to make case-insensitive.
+
+            if (Character.isLetter(letter)) { // bonus mission 3- remove non-alphanumerics
+
+                if (charMapCount.containsKey(letter)) {
+                    charMapCount.put(letter, charMapCount.get(letter) + 1);
+                } else {
+                    charMapCount.put(letter, 1);
+                }
             }
-        }
 
-        for (Map.Entry<Character, Integer> entry : charMapCount.entrySet()) {
-            System.out.println(entry.getKey()+" : "+entry.getValue());
+            for (Map.Entry<Character, Integer> entry : charMapCount.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+
+            }
         }
     }
 }
+    // remove all non-alphanumeric characters from the string
+
 
 // from previous KC class - does not work
 //public class CountingCharacters {
